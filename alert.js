a57affd5fc70d6abbd069bd88fd37273951da8b5
shoaibtahir97 <shoaibtahir97@gmail.com>
2021-04-07 23:59:59 +0500

    first assignment completed

---
+++ alert.js
@@ -0,0 +1,16 @@
+//1).
+alert("Welcome to our website!");
+
+//2).
+alert("Error! Please enter a valid password.");
+
+//3). 
+alert("Welcome to JS Land... \n Happy Coding!");
+
+//4).
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!" );
+
+//5).
+alert("Hello... I can run JS through my web browser's console");
